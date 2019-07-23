be93c0c6927483bf1159502079a98da830d13b8d
孤豹 <yongbao.fyb@koubei.com>
2019-07-23 22:39:57 +0800

    fix: add PUBLIC_PATH to DefinePlugin

--- webpack/config/other.js
+++ webpack/config/other.js
@@ -5,7 +5,8 @@ const webpack = require('webpack');
 const get = require('lodash/get');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const systemModulePlugin = require('@ali/saas-webpack-system-module');
-const { ROOT_PATH, SAAS_CONFIG } = require('../util/const');
+const { ROOT_PATH, SAAS_CONFIG, ASSETS_URL } = require('../util/const');
+
 const microAppName = get(SAAS_CONFIG, 'microAppName', '');
 
 module.exports = function(config){
@@ -15,5 +16,6 @@ module.exports = function(config){
   config.plugins.push(new CleanWebpackPlugin([path.join(ROOT_PATH, 'build')]));
   config.plugins.push(new webpack.DefinePlugin({
     'MICRO_APPNAME': JSON.stringify(microAppName),
+    'PUBLIC_PATH': JSON.stringify(ASSETS_URL),
   }));
 };
