1ee2eb4b79adad85dc34a23dc2dc04e4f9fa9f9c
孤豹 <yongbao.fyb@koubei.com>
2019-08-18 21:55:37 +0800

    fix: 添加线上构建校验

--- webpack/util/const.js
+++ webpack/util/const.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const chalk = require("chalk");
 const path = require('path');
 const url = require('url-join');
 const parse = require('yargs-parser');
@@ -15,7 +16,15 @@ const BUILD_GIT_PROJECT = BUILDER_ENV.BUILD_GIT_PROJECT;
 const BUILD_GIT_VERSION = BUILDER_ENV.BUILD_GIT_VERSION;
 
 // 取得当前是日常还是生产环境
-let argv = !!BUILDER_ENV.BUILD_ARGV_STR ? parse(BUILDER_ENV.BUILD_ARGV_STR) : [];
+// 日常： --def_publish_type=assets --def_publish_env=daily
+// 生产：
+console.log('获取构建环境：');
+console.log(BUILDER_ENV.BUILD_ARGV_STR);
+let argv = !!BUILDER_ENV.BUILD_ARGV_STR ? parse(BUILDER_ENV.BUILD_ARGV_STR) : {};
+if (!argv.def_publish_env) {
+  console.log(chalk.red('未开启线上构建, 参考文档：https://yuque.antfin-inc.com/alsc-saas/vt2tmg/ehge2p#UqnpK'));
+  process.exit(0);  
+}
 
 let CDN_BASE = '//g.alicdn.com/';
 if (argv.def_publish_env === 'daily') {
