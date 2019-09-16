219642fdd6369833a3779d27bd58825126b0b750
孤豹 <yongbao.fyb@koubei.com>
2019-09-16 20:41:56 +0800

    fix: edit layout

--- webpack/config/html.js
+++ webpack/config/html.js
@@ -19,7 +19,7 @@ module.exports = function (config, argv) {
 
   let layout = get(SAAS_CONFIG, 'layout', false);
   if (layout === true) {
-    layout = 'alsc-saas/web-boh-common/1.0.26';
+    layout = 'alsc-saas/web-boh-common/1.1.0';
   }
 
   microAppName = microAppName ? microAppName : 'bcommon';
