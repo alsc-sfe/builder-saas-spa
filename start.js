b91fec126ae51bbb79c6cf94c0f185b21fdccbb4
孤豹 <yongbao.fyb@koubei.com>
2019-09-24 21:24:23 +0800

    fix: add cors header

--- start.js
+++ start.js
@@ -65,6 +65,11 @@ const start = async () => {
     quiet: false,
     host: 'local.koubei.test',
     historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
   }
 
   // compile and start server
