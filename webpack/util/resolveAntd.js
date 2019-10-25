c182b2a004d1d1b926fda0392438b59300c23105
孤豹 <yongbao.fyb@koubei.com>
2019-10-25 16:29:23 +0800

    fix: add antd external logic

--- webpack/util/resolveAntd.js
+++ webpack/util/resolveAntd.js
@@ -13,12 +13,25 @@ const toHump = (name) => {
 }
 
 module.exports = function(alias) {
+  if (!fs.existsSync(path.join(__dirname, '../mock/antd'))) {
+    fs.mkdirSync(path.join(__dirname, '../mock/antd'));
+  }
+  const cssFileProxyPath = path.join(__dirname, '../mock/antd/empty.less');  
+  fs.writeFileSync(cssFileProxyPath, '');
+
   const antdFolder = path.join(__dirname, '../../node_modules/antd');
   const items = fs.readdirSync(path.join(antdFolder, 'lib'));
   items.filter(item => {
-    const styleFilePath = `antd/lib/${item}/style`;
-    alias[styleFilePath] = path.join(__dirname, '../mock/antdEmpty.less');
-    // const jsFilePath = `antd/lib/${item}`;
-    // alias[jsFilePath] = `window.antd.${toHump(item)}`;
+    if (!item.startsWith('_')) {
+      const styleFilePath = `antd/lib/${item}/style`;
+      alias[styleFilePath] = path.join(__dirname, '../mock/antd/empty.less');
+      const jsFilePath = `antd/lib/${item}`;
+      const jsFileProxyPath = path.join(__dirname, `../mock/antd/${item}.js`);  
+      fs.writeFileSync(jsFileProxyPath, `
+        const ${toHump(item)} = window.antd['${toHump(item)}'];
+        export { ${toHump(item)} as default };
+      `);
+      alias[jsFilePath] = jsFileProxyPath;
+    }
   });
 };
