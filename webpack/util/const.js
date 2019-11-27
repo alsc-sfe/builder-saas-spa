37026652550ab49543c67f0542da7247faf90acf
louisa.ly <louisa.ly@koubei.com>
2019-11-27 23:53:44 +0800

    fix assest url

--- webpack/util/const.js
+++ webpack/util/const.js
@@ -7,6 +7,7 @@ const parse = require('yargs-parser');
 const BUILDER_ENV = require('./env');
 const ROOT_PATH = process.cwd();
 const SAAS_CONFIG = require(path.join(ROOT_PATH, 'app.config.ts'));
+const PKG = require(path.join(ROOT_PATH, 'package.json'));
 const PLATFORM = process.platform;
 
 console.log('platform', PLATFORM);
@@ -16,8 +17,6 @@ const BUILD_ENV = BUILDER_ENV.BUILD_ENV;
 const SRC_PATH = path.resolve(ROOT_PATH, 'src');
 const BUILD_PATH = BUILDER_ENV.BUILD_DEST ? path.join(ROOT_PATH, BUILDER_ENV.BUILD_DEST) : path.resolve(ROOT_PATH, 'build');
 const BUILD_GIT_GROUP = BUILDER_ENV.BUILD_GIT_GROUP;
-const BUILD_GIT_PROJECT = BUILDER_ENV.BUILD_GIT_PROJECT;
-const BUILD_GIT_VERSION = BUILDER_ENV.BUILD_GIT_VERSION || '0.0.1';
 let PUBLISH_ENV = '';
 
 let argv = !!BUILDER_ENV.BUILD_ARGV_STR ? parse(BUILDER_ENV.BUILD_ARGV_STR) : {};
@@ -37,6 +36,8 @@ if (BUILD_ENV === 'cloud' && !argv.def_publish_env) {
 // 获取发布环境（daily、prod）
 // PUBLISH_ENV = argv.def_publish_env;
 PUBLISH_ENV = 'dev';
+const BUILD_GIT_PROJECT = PKG.name;
+const BUILD_GIT_VERSION = PKG.version || '0.0.1';
 const CDN_BASE_DAILY = '//bohstatic.dev.choicesaas.cn';
 const CDN_BASE_PROD = '//g.alicdn.com/';
 let CDN_BASE = CDN_BASE_DAILY;
