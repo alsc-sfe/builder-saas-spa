05622aca39a96442761ad7f10426cec7a9786327
louisa.ly <louisa.ly@koubei.com>
2020-04-30 01:34:15 +0800

    fix: webpack 报错

--- build.js
+++ build.js
@@ -4,9 +4,10 @@ const webpackBuildConfig = require("./webpack/webpack.build.config");
 const build = async () => {
   await webpack(webpackBuildConfig, (err, stats) => {
     if (err || stats.hasErrors()) {
-      console.log(stats);
-      console.log(err);
+      console.log('stats 构建出错',stats.toJson({errorDetails: false}));
+      console.log('err 构建出错', err.toString());
       console.log("构建过程出错！");
+      throw("构建过程出错！", err, stats);
     } else {
       console.log("构建成功！");
     }    
