234feee32de77942eb0c2218d25e7ec7482c26dc
louisa.ly <louisa.ly@koubei.com>
2020-05-11 10:04:52 +0800

    fix: update error中断

--- build.js
+++ build.js
@@ -4,10 +4,15 @@ const webpackBuildConfig = require("./webpack/webpack.build.config");
 const build = async () => {
   await webpack(webpackBuildConfig, (err, stats) => {
     if (err || stats.hasErrors()) {
-      console.log('stats 构建出错',stats.toJson({errorDetails: false}));
-      console.log('err 构建出错', err.toString());
-      console.log("构建过程出错！");
-      throw("构建过程出错！", err, stats);
+      console.error("构建过程出错！");
+      if (stats.hasErrors()) {
+        console.error('stats 构建出错', stats.toJson({ errorDetails: false }));
+      }
+      if (err) {
+        console.error('err 构建出错', err.message);
+      }
+      // 异常退出，反馈exitCode为1
+      process.exit(1);
     } else {
       console.log("构建成功！");
     }    
